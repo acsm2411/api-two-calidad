30891c7c06decb7902a61ebd317f28271fcf5c8a
Andres Silva <Andres.Silva@anuntatech.com>
2022-08-21 06:51:15 -0500

    Fix test string y no content

--- main_test.py
+++ main_test.py
@@ -8,6 +8,10 @@ def test_route():
     response = client.get('/authUsers/95092')
     assert response.status_code == 200
 
-def test_route_no_content():
+def test_route_string():
     response = client.get('/authUsers/test')
+    assert response.status_code == 422
+
+def test_route_no_content():
+    response = client.get('/authUsers/-1')
     assert response.status_code == 204
